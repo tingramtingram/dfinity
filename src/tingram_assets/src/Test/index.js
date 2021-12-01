4eeca5a5f51051b4a499840a070fd61a78d13b13
unknown <tingram.net@gmail.com>
2021-12-01 23:57:07 +0300

    backend2

--- src/tingram_assets/src/Test/index.js
+++
@@ -1,9 +0,0 @@
-import { tingram } from "../../declarations/tingram";
-
-document.getElementById("clickMeBtn").addEventListener("click", async () => {
-  const name = document.getElementById("name").value.toString();
-  // Interact with tingram actor, calling the greet method
-  const greeting = await tingram.greet(name);
-
-  document.getElementById("greeting").innerText = greeting;
-});
